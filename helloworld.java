61e07ac6862a2e5aa79502cf7185e1a157a0f14e
jorgevargasjvm <jorge.jvm.92@hotmail.com>
2019-12-26 03:25:48 -0400

    hello y Adios

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// Prints "Hello World" to the terminal windows.
-		System.out.println("Hello, World");
+		System.out.println("Hello");
 		System.out.println("Adios");
 	}
 }
